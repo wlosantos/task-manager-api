9a23b31a7ab3a1b3165e74b115648610db1cb331
Wendel Lopes <wendelopes@msn.com>
2021-04-20 19:22:27 -0300

    Desabling devise routes (temporarily)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ require 'api_version_constraint'
 
 Rails.application.routes.draw do
 
-  devise_for :users
+  # devise_for :users
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
     namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
       resources :users, only: %i[ show ]
