f9a504f9b32bc5ce0a52ca27cfbc1791f881b264
Wendel Lopes <wendelopes@msn.com>
2021-04-21 17:21:47 -0300

    Adding method info in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates_uniqueness_of :auth_token
+
+  def info
+    "#{email} - #{created_at}"
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,10 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_confirmation_of(:password) }
   it { is_expected.to allow_value('wendel@lopes.com').for(:email) }
   it { is_expected.to validate_uniqueness_of(:auth_token) }
+
+  describe '#info' do
+    it 'returns email and created_at' do
+      expect(user.info).to eq("#{user.email} - #{user.created_at}")
+    end
+  end
 end
