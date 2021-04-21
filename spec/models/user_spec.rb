da4156c01e68dc7fba13471d5f320dc12b8f74e1
Wendel Lopes <wendelopes@msn.com>
2021-04-21 19:06:23 -0300

    Updating user#info

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,8 +11,11 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_uniqueness_of(:auth_token) }
 
   describe '#info' do
-    it 'returns email and created_at' do
-      expect(user.info).to eq("#{user.email} - #{user.created_at}")
+    it 'returns email, created_at and a Token' do
+      user.save!
+      allow(Devise).to receive(:friendly_token).and_return('abc123TOKEN')
+
+      expect(user.info).to eq("#{user.email} - #{user.created_at} - Token: abc123TOKEN")
     end
   end
 end
