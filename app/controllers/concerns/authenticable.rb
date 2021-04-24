943ac0f5b2cd626b5523b94e0debd462baef168e
Wendel Lopes <wendelopes@msn.com>
2021-04-24 11:38:21 -0300

    Adding #Authenticate_with_token! method

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -4,4 +4,8 @@ module Authenticable
     @current_user ||= User.find_by(auth_token: request.headers['Authorization'])
   end
 
+  def authenticate_with_token!
+    render json: { errors: 'Unauthorized access!' }, status: 401 unless current_user.present?
+  end
+
 end
--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -6,4 +6,29 @@ RSpec.describe Authenticable, type: :controller do
     include Authenticable
   end
 
+  let(:app_controller) { subject }
+
+  describe '#authenticate_with_token!' do
+    controller do
+      before_action :authenticate_with_token!
+
+      def restricted_ation; end
+    end
+
+    context 'when there is no user logged in' do
+      before do
+        allow(app_controller).to receive(:current_user).and_return(nil)
+        routes.draw { get 'restricted_ation' => 'anonymous#restricted_ation' }
+        get :restricted_ation
+      end
+
+      it 'returns status code 401' do
+        expect(response).to have_http_status(401)
+      end
+      it 'returns the json data for the errors' do
+        expect(json_body).to have_key(:errors)
+      end
+    end
+  end
+
 end
