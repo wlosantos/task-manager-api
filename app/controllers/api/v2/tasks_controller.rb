58f67dd62a0a6bf649591ae271fc72697ef22f51
Wendel Lopes <wendelopes@msn.com>
2021-04-26 20:06:15 -0300

    Adding API::v2

---
+++ app/controllers/api/v2/tasks_controller.rb
@@ -0,0 +1,53 @@
+class Api::V2::TasksController < ApplicationController
+
+  before_action :authenticate_with_token!
+  before_action :set_task, only: %i[ update destroy ]
+
+  def index
+    tasks = current_user.tasks.all
+    render json: { tasks: tasks}, status: :ok
+  end
+
+  def show
+    begin
+      task = current_user.tasks.find_by(id: params[:id])
+      render json: task, status: :ok
+    rescue
+      head 404
+    end
+  end
+
+  def create
+    task = current_user.tasks.build(task_params)
+
+    if task.save
+      render json: task, status: :created
+    else
+      render json: { errors: task.errors }, status: 422
+    end
+  end
+
+  def update
+    if @task.update(task_params)
+      render json: @task, status: 200
+    else
+      render json: { errors: @task.errors }, status: 422
+    end
+  end
+
+  def destroy
+    @task.destroy
+    head 204
+  end
+
+  private
+
+  def set_task
+    @task = current_user.tasks.find_by(id: params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description, :done, :deadline)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,13 @@ Rails.application.routes.draw do
   devise_for :users, only: %i[ sessions ], controller: { sessions: 'api/v1/sessions' }
 
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
-    namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
+    namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1) do
+      resources :users, only: %i[ show create update destroy ]
+      resources :sessions, only: %i[ create destroy ]
+      resources :tasks, only: %i[ index show create update destroy ]
+    end
+
+    namespace :v2, path: '/', constraints: ApiVersionConstraint.new(version: 2, default: true) do
       resources :users, only: %i[ show create update destroy ]
       resources :sessions, only: %i[ create destroy ]
       resources :tasks, only: %i[ index show create update destroy ]
